eb797e8267428183d47f522e075aaba6513f176b
ayu1298 <theinvincible375@gmail.com>
2020-12-09 19:13:40 +0530

    Getting info from Parse Server

--- app/src/main/java/com/example/instagramclone/SignUp.java
+++ app/src/main/java/com/example/instagramclone/SignUp.java
@@ -4,20 +4,40 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.parse.GetCallback;
 import com.parse.Parse;
 import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.SaveCallback;
 import com.shashank.sony.fancytoastlib.FancyToast;
 
 public class SignUp extends AppCompatActivity {
 
+    private TextView txtGetData;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        txtGetData=findViewById(R.id.txtGetdata);
+        txtGetData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ParseQuery<ParseObject> parseQuery=ParseQuery.getQuery("Boxer");
+                parseQuery.getInBackground("QdhkSIr7td", new GetCallback<ParseObject>() {
+                    @Override
+                    public void done(ParseObject object, ParseException e) {
+                        if(object!=null && e==null)
+                        {
+                            txtGetData.setText(object.get("punch_speed") + "");
+                        }
+                    }
+                });
+            }
+        });
 
     }
     public void hello(View view)
