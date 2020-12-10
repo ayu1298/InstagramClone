68e614b261c8b01e9ddfe58398f9c6ef7398f327
ayu1298 <theinvincible375@gmail.com>
2020-12-10 16:39:59 +0530

    Insta Login and Sign Up

--- app/src/main/java/com/example/instagramclone/SignUp.java
+++ app/src/main/java/com/example/instagramclone/SignUp.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,77 +21,40 @@ import com.shashank.sony.fancytoastlib.FancyToast;
 
 import java.util.List;
 
-public class SignUp extends AppCompatActivity {
+public class SignUp extends AppCompatActivity implements View.OnClickListener {
 
-    private TextView txtGetData;
-    private Button btnGetAll,btnTransition;
-    private String allBoxers;
+    private EditText edtEmail,edtUsername,edtPassword;
+    private Button btnSignUp,btnLogin;
+   // private String allBoxers;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-        txtGetData = findViewById(R.id.txtGetdata);
-        btnGetAll = findViewById(R.id.btnGetAll);
-        btnTransition = findViewById(R.id.btnNextActivity);
+        setTitle("Sign Up");
 
-        txtGetData.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                ParseQuery<ParseObject> parseQuery = ParseQuery.getQuery("Boxer");
-                parseQuery.getInBackground("QdhkSIr7td", new GetCallback<ParseObject>() {
-                    @Override
-                    public void done(ParseObject object, ParseException e) {
-                        if (object != null && e == null) {
-                            txtGetData.setText(object.get("punch_speed") + "");
-                        }
-                    }
-                });
-            }
-        });//To get all data at once
-        btnGetAll.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                allBoxers = "";
-                ParseQuery<ParseObject> queryAll = ParseQuery.getQuery("Boxer");
-                queryAll.whereGreaterThan("punch_speed", 100);
-                queryAll.setLimit(1);
-                queryAll.findInBackground(new FindCallback<ParseObject>() {
-                    @Override
-                    public void done(List<ParseObject> objects, ParseException e) {
-                        if (e == null) {
-                            if (objects.size() > 0) {
-                                for (ParseObject parseObject : objects)
-                                    allBoxers += parseObject.get("punch_speed") + "\n";
-                                FancyToast.makeText(SignUp.this, allBoxers, FancyToast.LENGTH_LONG, FancyToast.SUCCESS, true).show();
-                            } else {
-                                FancyToast.makeText(SignUp.this, "Failed", FancyToast.LENGTH_LONG, FancyToast.ERROR, true).show();
-                            }
-                        }
-                    }
-                });
-            }
-        });
+        edtEmail=findViewById(R.id.edtEnterEmail);
+        edtUsername=findViewById(R.id.edtUsername);
+        edtPassword=findViewById(R.id.edtPassword);
+
+        btnSignUp=findViewById(R.id.button);
+        btnLogin=findViewById(R.id.btnLogIn);
+
+        btnSignUp.setOnClickListener(this);
+        btnLogin.setOnClickListener(this);
 
 
-        btnTransition.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(SignUp.this,SignUpLoginActivity.class);
-                startActivity(intent);
-            }
-        });
     }
 
-    public void hello(View view)
-    {
-        ParseObject boxer=new ParseObject("Boxer");
-        boxer.put("punch_speed",200);
-        boxer.saveInBackground(new SaveCallback() {
-            @Override
-            public void done(ParseException e) {
-                if(e==null)
-                    FancyToast.makeText(SignUp.this,"Boxer saved",FancyToast.LENGTH_LONG,FancyToast.SUCCESS,true).show();
-            }
-        });
+    @Override
+    public void onClick(View view) {
+        switch (view.getId())
+        {
+            case R.id.button:
+
+                break;
+            case R.id.btnLogIn:
+                break;
+        }
+
     }
 }
