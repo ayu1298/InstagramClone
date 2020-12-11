170c012993d8c3a68acde6f2ec7b358a2d153111
ayu1298 <theinvincible375@gmail.com>
2020-12-11 19:27:46 +0530

    share images all implemented

--- app/src/main/java/com/example/instagramclone/SharePictureTab.java
+++ app/src/main/java/com/example/instagramclone/SharePictureTab.java
@@ -1,19 +1,44 @@
 package com.example.instagramclone;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.Fragment;
+
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.shashank.sony.fancytoastlib.FancyToast;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.ByteArrayOutputStream;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link SharePictureTab#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class SharePictureTab extends Fragment {
+   public class SharePictureTab extends Fragment implements View.OnClickListener {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,6 +49,10 @@ public class SharePictureTab extends Fragment {
     private String mParam1;
     private String mParam2;
 
+    private ImageView imgShare;
+    private EditText edtDescrption;
+    private Button btnShareImage;
+    Bitmap receivedImageBitmap;
     public SharePictureTab() {
         // Required empty public constructor
     }
@@ -59,6 +88,108 @@ public class SharePictureTab extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_share_picture_tab, container, false);
+
+        View view= inflater.inflate(R.layout.fragment_share_picture_tab, container, false);
+        imgShare=view.findViewById(R.id.imageView3);
+         edtDescrption=view.findViewById(R.id.editTextTextPersonName);
+         btnShareImage=view.findViewById(R.id.btnShareImage);
+         imgShare.setOnClickListener(SharePictureTab.this);
+         btnShareImage.setOnClickListener(SharePictureTab.this);
+         return view;
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId())
+        {
+            case R.id.imageView3:
+                  if(Build.VERSION.SDK_INT>=21 && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.READ_EXTERNAL_STORAGE
+                  )!= PackageManager.PERMISSION_GRANTED)
+                 {
+                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},1000);
+                  }
+                  else
+                  {
+                      getChosenImage();
+                  }
+
+                break;
+            case R.id.btnShareImage:
+
+                  if(receivedImageBitmap!=null)
+                  {
+                      if(edtDescrption.getText().toString().equals(""))
+                      {
+                          FancyToast.makeText(getContext(),"Error, You must specify a descrpition", Toast.LENGTH_SHORT,FancyToast.ERROR,true).show();
+                      }
+                      else
+                      {
+                          ByteArrayOutputStream byteArrayOutputStream=new ByteArrayOutputStream();
+                          receivedImageBitmap.compress(Bitmap.CompressFormat.PNG,100,byteArrayOutputStream);
+                          byte[] bytes=byteArrayOutputStream.toByteArray();
+                          ParseFile parseFile=new ParseFile("pic.png",bytes);
+                          ParseObject parseObject=new ParseObject("Photo");
+                          parseObject.put("picture",parseFile);
+                          parseObject.put("image_des",edtDescrption.getText().toString());
+                          parseObject.put("username", ParseUser.getCurrentUser().getUsername());
+
+                      }
+                  }
+                  else
+                  {
+                      FancyToast.makeText(getContext(),"Error, You must select an image", Toast.LENGTH_SHORT,FancyToast.ERROR,true).show();
+                  }
+                break;
+
+
+        }
+
+    }
+    // this function is called when permissions given
+    private void getChosenImage() {
+
+       //FancyToast.makeText(SharePictureTab.this,"Now we can access the images", Toast.LENGTH_SHORT,FancyToast.SUCCESS,true).show();
+        Intent intent=new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent,2000);
+    }
+
+    @Override// to check if permission granted is received
+    public void onRequestPermissionsResult(int requestCode, @NonNull @NotNull String[] permissions, @NonNull @NotNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode==1000)
+        {
+            if(grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED)
+            {
+                getChosenImage();
+            }
+        }
+    }
+
+    @Override  //to access gallery,camera for images
+    public void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==2000)
+        {   if(resultCode==Activity.RESULT_OK)
+            // do something with your captured image
+            try {
+                {
+                    Uri selectedImage=data.getData();
+                    String[] filePathColumn={MediaStore.Images.Media.DATA};
+                    Cursor cursor=getActivity().getContentResolver().query(selectedImage,filePathColumn,null,null,null);
+                    cursor.moveToFirst();
+                    int columnIndex=cursor.getColumnIndex(filePathColumn[0]);
+                    String picturePath=cursor.getString(columnIndex);
+                    cursor.close();
+                    receivedImageBitmap= BitmapFactory.decodeFile(picturePath);
+                    imgShare.setImageBitmap(receivedImageBitmap);
+                }
+            }
+        catch (Exception e)
+        {
+            e.getStackTrace();
+        }
+        }
     }
 }
